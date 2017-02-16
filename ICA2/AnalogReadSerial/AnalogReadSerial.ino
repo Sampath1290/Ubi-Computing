93e82ea0ea49af1883c094a6347058fe10869d7d
jpholloway8824 <jpholloway8824@gmail.com>
2017-02-16 15:14:08 -0600

    complete ICA2

--- ICA2/AnalogReadSerial/AnalogReadSerial.ino
+++ ICA2/AnalogReadSerial/AnalogReadSerial.ino
@@ -1,13 +1,14 @@
 // the setup routine runs once when you press reset:
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(57600);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
   // read the input on analog pin A3:
   int sensorValue = analogRead(A3);
+  uint8_t new_val = map(sensorValue, 0, 4096, 0, 255);
   // print out the value you read:
   Serial.write(new_val);
-  delay(100); // delay in between reads for stability
+  delay(1); // delay in between reads for stability
 }
